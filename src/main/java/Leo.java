3700a5c519b1e4300da6502d08c7feb839c8b8ec
Starishine <sania.amarnath@gmail.com>
2025-08-16 19:55:08 +0800

    Echos user commands

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class Leo {
     public static void main(String[] args) {
         // greets the user
+        printLogo();
         printLine();
-        System.out.println("Hello, I'm Leo");
-        System.out.println("What can I do for you?");
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello, \uD83C\uDF1F I'm Leo, your favorite chatbot!");
+        System.out.println("What can I do for you today?");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        // echoes user input until "bye" is entered
+        while (!input.equals("bye")){
+            System.out.println("You said: " + input);
+            input = scanner.next();
+        }
+        System.out.println("Bye \uD83D\uDC4B ! Hope to see you soon!");
+        scanner.close();
     }
 
     public static void printLine() {
         System.out.println("___________________________________________");
+    }
+
+    public static void printLogo(){
+        System.out.println("██╗     ███████╗ ██████╗ ");
+        System.out.println("██║     ██╔════╝██╔═══██╗");
+        System.out.println("██║     █████╗  ██║   ██║");
+        System.out.println("██║     ██╔══╝  ██║   ██║");
+        System.out.println("███████╗███████╗╚██████╔╝");
+        System.out.println("╚══════╝╚══════╝ ╚═════╝ ");
+        System.out.println();
     }
 }
