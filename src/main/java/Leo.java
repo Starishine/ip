7fa2d8f6b39583a190652d859aa20fcee3bc88e4
Starishine <sania.amarnath@gmail.com>
2025-08-16 18:52:37 +0800

    Rename and greet user

---
+++ src/main/java/Leo.java
@@ -0,0 +1,14 @@
+public class Leo {
+    public static void main(String[] args) {
+        // greets the user
+        printLine();
+        System.out.println("Hello, I'm Leo");
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printLine() {
+        System.out.println("___________________________________________");
+    }
+}
